91918b00c43532b56b0292b52446f3f4dda10d4b
phimage <eric.marchand.n7@gmail.com>
2019-06-10 07:54:18 +0200

    Do not use `URLErrorDomain`. Use our own domain. Fix #509

--- Sources/OAuthSwiftURLHandlerType.swift
+++ Sources/OAuthSwiftURLHandlerType.swift
@@ -161,7 +161,7 @@ import AuthenticationServices
             let key = UUID().uuidString
 
             observers[key] = OAuthSwift.notificationCenter.addObserver(
-                forName: NSNotification.Name.OAuthSwiftHandleCallbackURL,
+                forName: OAuthSwift.didHandleCallbackURL,
                 object: nil,
                 queue: OperationQueue.main,
                 using: { [weak self] _ in
